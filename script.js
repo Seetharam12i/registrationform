17da488a649a5d85505823b28fa0f107cf2cd4c7
Seetharam12i <140388177+Seetharam12i@users.noreply.github.com>
2025-02-17 17:46:18 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('registrationForm').addEventListener('submit', function(event) 
 {
-    event.preventDefault(); // Prevent the form from submitting
+    event.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
